04f005e4133103a2c71ed8c30b4f828f8c1440f9
jingpeng <peng.jing@maxent-inc.com>
2016-06-02 16:49:02 +0800

    fix bug of logo title display  error

--- src/components/App.js
+++ src/components/App.js
@@ -44,8 +44,7 @@ var Logo=React.createClass({
                      style={{width:"138px",height:"40px",backgroundColor:"#45A2E1",float:"right"}}>
                     <div style={{height:"60%",fontSize:"20px",color:"white"}}>中国电信</div>
                     <div
-                        style={{height:"40%",fontSize:"12px",color:"white"}}> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        ——&nbsp;&nbsp;服务监控平台
+                        style={{height:"40%",fontSize:"12px",color:"white"}}>服务监控平台<span style={{fontStyle:"italic",color:"#D3D3D3"}}>&nbsp;Alpha 0.1</span>
                     </div>
                 </div>
             </div>
