579c8105335bb77a131e7a5d07717f881c6e109a
jingpeng <peng.jing@maxent-inc.com>
2016-06-05 19:09:38 +0800

    add router to project

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,8 +7,10 @@ var jQuery = require('jquery');
 var Dashboard = React.createClass({
     render: function () {
         return (
-            <div>
-                Dashboard
+            <div className="cl-mcont" style={{padding:"10px 0px"}}>
+                <div style={{padding:"0px 0px 0px 6px"}}>
+                    Dashboard
+                </div>
             </div>
         )
 
