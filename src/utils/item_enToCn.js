df7b7d2fe90a1df40f499110a44d42c837106f99
jingpeng <peng.jing@maxent-inc.com>
2016-06-20 17:46:46 +0800

    merge

--- src/utils/item_enToCn.js
+++ src/utils/item_enToCn.js
@@ -2,16 +2,60 @@
  * Created by Captain on 2016/6/20.
  */
 enToCn_data = {
-    normal_status: [
-        {
-            key: "vmware.eventlog[{$URL}]",
-            value: {
-                name: "Event log",
-                name_cn: "事件日志",
-                position: "center",
-                priority: 3
-            }
+    normal_status: {
+        "vmware.eventlog[{$URL}]": {
+            name: "Event log",
+            name_cn: "事件日志",
+            position: "center",
+            priority: 3
+        },
+        23668: {
+            name: "Full name",
+            name_cn: "全名",
+            position: "center",
+            priority: 1
+        },
+        23669: {
+            name: "Version",
+            name_cn: "版本",
+            position: "center",
+            priority: 2
         }
-    ]
+    },
+    cpu: {
+        23733: {
+            name: "CPU cores",
+            name_cn: "cpu核数",
+            position: "center"
+        },
+        23731: {
+            name: "CPU frequency",
+            name_cn: "cpu主频",
+            position: "center"
+        },
+        23732: {
+            name: "CPU model",
+            name_cn: "cpu型号",
+            position: "center"
+        },
+        23734: {
+            name: "CPU threads",
+            name_cn: "cpu线程数",
+            position: "center"
+        },
+        23726: {
+            name: "CPU usage",
+            name_cn: "cpu使用率",
+            position: "center",
+            show_method: "scroll"
+        }
+    },
+    noTab: {
+        empty: {
+            name: "未知",
+            name_cn: "未知",
+            position: "center"
+        }
+    }
 
 };
